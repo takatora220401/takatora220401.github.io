b2f9c35a38fe9cbe2cb577ee14a4c6420c38dc56
takatora220401 <chromosome.somen@gmail.com>
2026-02-06 01:01:35 +0900

    msg

--- commitAndPush.sh
+++ commitAndPush.sh
@@ -4,6 +4,6 @@ git add . && \
 git commit -m "msg" && \
 git push
 
-#<p><strong>phase</strong><\p>    ==> used to emphasize keywords inside <p>
+#<li><strong>phase</strong><\li>    ==> used to emphasize keywords inside <li>
 #<pre><code> </code></pre> ==> used for inserting codes with black background
 #<blockquote><\blockquote> ==> used to quoate with gray background
